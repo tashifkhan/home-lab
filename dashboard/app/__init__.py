4bfeccd0f6b61f1d13c6d8b4e40ace378e4ae911
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2026-03-27 02:24:19 +0530

    added logo

--- dashboard/app/__init__.py
+++ dashboard/app/__init__.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from .routers import auth, dashboard, health, tailscale
 
 
 def create_app() -> FastAPI:
     app = FastAPI(docs_url=None, redoc_url=None)
+    app.mount("/static", StaticFiles(directory="static"), name="static")
     app.include_router(auth.router)
     app.include_router(dashboard.router)
     app.include_router(health.router)
